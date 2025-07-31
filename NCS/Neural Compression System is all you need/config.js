b3d86d4af06af35f905fb2c8c658e012a3c297b9
Yann Guszkiewicz <yannougus@gmail.com>
2025-07-31 14:07:27 +0200

    Create config.js

---
+++ NCS/Neural Compression System is all you need/config.js
@@ -0,0 +1,11 @@
+{
+  "device": "cuda",
+  "batch_size": 64,
+  "epochs": 3,
+  "learning_rate": 0.001,
+  "vqvae_model_path": "assets/models/vqvae_128.pt",
+  "token_path": "assets/tokens/TOKEN.npy",
+  "results_path": "outputs/results.json",
+  "baseline_model_path": "outputs/baseline_model.pt",
+  "token_model_path": "outputs/token_model.pt"
+}
