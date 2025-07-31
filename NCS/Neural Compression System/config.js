933240996e1618b25dca31aacd7ddf9ce910763b
Yann Guszkiewicz <yannougus@gmail.com>
2025-07-31 12:23:30 +0200

    Create config.js

---
+++ NCS/Neural Compression System/config.js
@@ -0,0 +1,12 @@
+{
+  "codebook_sizes": [64, 128, 256, 512],
+  "epochs": 3,
+  "batch_size": 64,
+  "learning_rate": 0.0002,
+  "device": "cuda",
+  "num_eval_images": 100,
+  "data_dir": "./data/mnist",
+  "model_dir": "outputs/models",
+  "recon_dir": "outputs/reconstructions",
+  "token_path": "outputs/tokens/TOKEN.json"
+}
