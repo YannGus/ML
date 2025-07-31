c8f2a97a24480b5a5e7eaf0f67a23c69843d88d9
Yann Guszkiewicz <yannougus@gmail.com>
2025-07-31 14:14:57 +0200

    Create model.py

---
+++ NCS/Neural Compression System is all you need/baseline_classifier/model.py
@@ -0,0 +1,23 @@
+import torch.nn.functional as F
+import torch.nn as nn
+
+class CNNClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 32, 3, 1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 3, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Flatten()
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(9216, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        return self.fc(x)
